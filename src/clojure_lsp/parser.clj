d2eeb28664219b4375bd05335ac2cf4412cbb0c9
Eric Dallo <ericdallo06@hotmail.com>
2020-08-19 22:15:08 -0300

    Fix clean-ns for unused refers

--- src/clojure_lsp/parser.clj
+++ src/clojure_lsp/parser.clj
@@ -444,7 +444,8 @@
                     referee (n/string refer-node)]]
         (vswap! context update :refers assoc referee refered)
         (add-reference context scoped refer-node
-                       {:tags #{:refer}
+                       {:tags #{:declare :alias :refer}
+                        :ns full-ns
                         :sym refered})))))
 
 (defn add-libspecs [libtype context scoped entry-loc prefix-ns]
